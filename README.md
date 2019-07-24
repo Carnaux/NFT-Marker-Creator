de8a3341dd855f46e1c133af433506c5bd328839
Daniel Fernandes Gonçalves de Oliveira <danielfeenandes@gmail.com>
2019-07-23 22:45:39 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,12 @@
+# NFT MARKER EDITOR 
+
+This editor creates NFT markers for ARTOOKIT 5.x
+
+and it is part of the efforts to bring NFT tracking to AR.js
+
+| To Do                | %   |
+|----------------------|-----|
+| HTML UI              | 50% |
+| Porting genTexData.c | 10% |
+|                      |     |
+|                      |     |
---
+++ index.js
@@ -0,0 +1,30 @@
+var imageLoader = document.getElementById('imageLoader');
+    imageLoader.addEventListener('change', handleImage, false);
+var canvas = document.getElementById('imageCanvas');
+var ctx = canvas.getContext('2d');
+let imgData;
+
+function handleImage(e){
+    var reader = new FileReader();
+    reader.onload = function(event){
+        var img = new Image();
+        img.onload = function(){
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img,0,0);
+            let imageData = ctx.getImageData(0,0,canvas.width,canvas.height);
+            readImage(imageData);
+        }
+        img.src = event.target.result;
+    }
+    reader.readAsDataURL(e.target.files[0]); 
+    
+    
+
+   
+}
+
+function readImage(imageData){
+    imgData = imageData;
+    console.log(imgData);
+}
