d592cd2749a6a9b75e6bf48b0e487dc5b2454b98
Carnaux <danielfeenandes@gmail.com>
2019-12-12 14:50:04 -0300

    Solved 'value' undefined

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require("path");
 const fs = require('fs');
 const glob = require('glob');
 const inkjet = require('inkjet');
+const readlineSync = require('readline-sync');
 var Module = require('./NftMarkerCreator.min.js');
 
 let srcTest = path.join(__dirname, '/input/');
@@ -57,16 +58,55 @@ inkjet.exif(buf, function(err, metadata) {
         console.log("\n" + err + "\n");
         process.exit(1);
     }else{
+        // console.log(metadata)
         let dpi = Math.min(parseInt(metadata.XResolution.value), parseInt(metadata.YResolution.value));
 
         if(dpi == null || dpi == undefined || dpi == NaN){
             console.log("\nWARNING: No DPI value found! Using 72 as default value!\n")
             dpi = 72;
         }
-    
-        imageData.sizeX = metadata.ImageWidth.value;
-        imageData.sizeY = metadata.ImageLength.value;
-        imageData.nc = metadata.SamplesPerPixel.value;
+        
+        if(metadata.ImageWidth == null || metadata.ImageWidth == undefined){
+            if(metadata. PixelXDimension == null || metadata. PixelXDimension == undefined){
+                var answer = readlineSync.question('The image does not contain any width or height info, do you want to inform them?[y/n]\n');
+                
+                if(answer == "y"){
+                    var answer2 = readlineSync.question('Inform the width and height: e.g W=200 H=400\n');
+
+                    let vals = getValues(answer2, "wh");
+                    imageData.sizeX = vals.w;
+                    imageData.sizeY = vals.h;
+                }else{
+                    console.log("It's not possible to proceed without width or height info!")
+                    process.exit(1);
+                }
+            }else{
+                imageData.sizeX = metadata. PixelXDimension.value;
+                imageData.sizeY = metadata. PixelYDimension.value;
+            }
+        }else{
+            imageData.sizeX = metadata.ImageWidth.value;
+            imageData.sizeY = metadata.ImageLength.value;
+        }
+
+        if(metadata.SamplesPerPixel == null){
+           
+            var answer = readlineSync.question('The image does not contain the number of channels(nc), do you want to inform it?[y/n]\n');
+            
+            if(answer == "y"){
+                var answer2 = readlineSync.question('Inform the number of channels(nc):(black and white images have NC=1, colored images have NC=3) e.g NC=3 \n');
+
+                let vals = getValues(answer2, "nc");
+                imageData.nc = vals;
+            }else{
+                console.log("It's not possible to proceed without the number of channels!")
+                process.exit(1);
+            }
+           
+        }else{
+            imageData.nc = metadata.SamplesPerPixel.value;
+        }
+
         imageData.dpi = dpi;
     }
 });
@@ -98,3 +138,29 @@ let contentFset3 = Module.FS.readFile( filenameFset3);
 fs.writeFileSync( path.join(__dirname, '/output/') + filenameIset, content);
 fs.writeFileSync( path.join(__dirname, '/output/') + filenameFset, contentFset);
 fs.writeFileSync( path.join(__dirname, '/output/') + filenameFset3, contentFset3);
+
+
+function getValues(str, type){
+    let values;
+    if(type == "wh"){
+        let Wstr = "W=";
+        let Hstr = "H=";
+        var doesContainW = str.indexOf(Wstr);
+        var doesContainH = str.indexOf(Hstr);
+    
+        let valW = parseInt(str.slice(doesContainW+2, doesContainH));
+        let valH = parseInt(str.slice(doesContainH+2));
+    
+        values = {
+            w: valW,
+            h: valH
+        }
+    }else if(type == "nc"){
+        let nc = "NC=";
+        var doesContainNC = str.indexOf(nc);
+        values = parseInt(str.slice(doesContainNC+3));
+    }
+    
+    
+    return values;
+}
